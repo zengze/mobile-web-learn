695ba0b9bf844cf3f2f601fcb40cdfc49329344f
zengze <zengze@smart>
2017-11-29 14:15:15 +0800

    添加工作文件

---
+++ src/entry/work.jsx
@@ -0,0 +1,5 @@
+import { render } from 'react-dom';
+
+import Work from '../page/Work.jsx';
+
+render(<Work />, document.getElementById('react-content'));
---
+++ src/page/Work.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import { TabBar } from 'antd-mobile';
+
+export default class Home extends Component {
+
+  render() {
+
+    return (
+      <div>
+        我是工作
+      </div>
+    );
+  }
+}
